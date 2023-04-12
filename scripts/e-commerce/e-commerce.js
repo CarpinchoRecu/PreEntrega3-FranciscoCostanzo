ed10ce980341c46323486f5e8228310b3425bc79
CarpinchoRecu <123523494+CarpinchoRecu@users.noreply.github.com>
2023-04-12 00:10:21 -0300

    .

--- scripts/e-commerce/e-commerce.js
+++ scripts/e-commerce/e-commerce.js
@@ -1,23 +1,25 @@
-const btnAggCartList = document.querySelectorAll('.btnAggCart');
-const displayData = document.querySelector('#displayData');
-const totalDisplay = document.querySelector('#totalPrice');
+const btnAggCartList = document.querySelectorAll(".btnAggCart");
+const displayData = document.querySelector("#displayData");
+const totalDisplay = document.querySelector("#totalPrice");
 let total = 0;
 
 function searchPhones(id) {
-  const phone = Phones.find(phone => phone.id === id);
+  const phone = Phones.find((phone) => phone.id === id);
   if (phone) {
     const { model, price } = phone;
-    const data = `Modelo: ${model}, Precio: $${price.toFixed(2)}`;
-    displayData.innerHTML += data + "<br>";
-    total += price;
-    totalDisplay.innerHTML = `Total: $${total.toFixed(2)}`;
-  } else {
-    console.log(`No se encontró el teléfono con ID ${id}`);
+    const phoneItem = document.createElement("div");
+    phoneItem.innerHTML = `<span>Modelo: ${model}</span><br><span>Precio: $${price.toFixed(2)}</span>`;
+    const confirmAdd = confirm("¿Seguro que quiere agregar al Carrito?");
+    if (confirmAdd) {
+      displayData.appendChild(phoneItem);
+      total += price;
+      totalDisplay.innerHTML = `Total: $${total.toFixed(2)}`;
+    }
   }
 }
 
-btnAggCartList.forEach(btnAggCart => {
-  btnAggCart.addEventListener('click', () => {
+btnAggCartList.forEach((btnAggCart) => {
+  btnAggCart.addEventListener("click", () => {
     const phoneId = btnAggCart.dataset.phoneId;
     searchPhones(parseInt(phoneId));
   });
--- scripts/ui_components_script/card-sale.js
+++ scripts/ui_components_script/card-sale.js
@@ -1,8 +1,11 @@
-const cardsale = document.querySelector('.card-sale');
-cardsale.addEventListener('click', function () {
-    if (this.getAttribute('data-state') === 'front') {
-        this.setAttribute('data-state', 'back');
-    } else {
-        this.setAttribute('data-state', 'front');
-    }
+const cardsSale = document.querySelectorAll(".card-sale");
+
+cardsSale.forEach((cardsSale) => {
+    cardsSale.addEventListener("click", function () {
+        if (this.getAttribute("data-state") === "front") {
+            this.setAttribute("data-state", "back");
+        } else {
+            this.setAttribute("data-state", "front");
+        }
+    });
 });
