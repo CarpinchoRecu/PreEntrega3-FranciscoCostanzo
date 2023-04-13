f83327d33e8dd16dfe57217b89c4dd0ea34415d9
CarpinchoRecu <123523494+CarpinchoRecu@users.noreply.github.com>
2023-04-12 23:13:35 -0300

    .agrego boton eliminar

--- scripts/e-commerce/e-commerce.js
+++ scripts/e-commerce/e-commerce.js
@@ -6,9 +6,14 @@ let total = 0;
 function searchPhones(id) {
   const phone = Phones.find((phone) => phone.id === id);
   if (phone) {
-    const { model, price } = phone;
+    const { id, model, price } = phone;
     const phoneItem = document.createElement("div");
-    phoneItem.innerHTML = `<span>Modelo: ${model}</span><br><span>Precio: $${price.toFixed(2)}</span>`;
+    phoneItem.innerHTML = `
+      <span>Modelo: ${model}</span>
+      <br>
+      <span>Precio: $${price.toFixed(2)}</span>
+      <button class="removeBtn" data-phone-id="${id}">X</button>
+    `;
     const confirmAdd = confirm("¿Seguro que quiere agregar al Carrito?");
     if (confirmAdd) {
       displayData.appendChild(phoneItem);
@@ -23,4 +28,18 @@ btnAggCartList.forEach((btnAggCart) => {
     const phoneId = btnAggCart.dataset.phoneId;
     searchPhones(parseInt(phoneId));
   });
+});
+
+displayData.addEventListener("click", (event) => {
+  if (event.target.classList.contains("removeBtn")) {
+    const confirmRemove = confirm("¿Seguro que quiere eliminar este teléfono del Carrito?");
+    if (confirmRemove) {
+      const phoneId = event.target.dataset.phoneId;
+      const phoneItem = event.target.parentNode;
+      const phonePrice = Phones.find((phone) => phone.id === parseInt(phoneId)).price;
+      total -= phonePrice;
+      totalDisplay.innerHTML = `Total: $${total.toFixed(2)}`;
+      phoneItem.parentNode.removeChild(phoneItem);
+    }
+  }
 });
